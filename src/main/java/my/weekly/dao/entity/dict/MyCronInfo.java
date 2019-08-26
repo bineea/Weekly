2bae86ae5b411a3fc273da3d47e2d39bc46c99b3
bineea <33547731+bineea@users.noreply.github.com>
2019-08-27 01:40:38 +0800

    配置定时任务

--- src/main/java/my/weekly/dao/entity/dict/MyCronInfo.java
+++ src/main/java/my/weekly/dao/entity/dict/MyCronInfo.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum MyCronInfo {
 
-    PER_FIVE_SEC("0/5 * * * * ?", "每5秒执行") {},
+    PER_FIVE_SEC("0/5 * * * * ?", "每5秒钟执行一次") {},
+    PER_TEN_MINUTE("0 0/10 * * * ?", "每10分钟执行一次") {},
+    PER_ONE_HOUR("0 0 0/1 * * ?", "每1小时执行一次") {},
     ;
 
     @Getter
--- src/main/java/my/weekly/dao/entity/dict/QuartzInfo.java
+++ src/main/java/my/weekly/dao/entity/dict/QuartzInfo.java
@@ -2,8 +2,10 @@ package my.weekly.dao.entity.dict;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import my.weekly.manager.quartz.ClearInvalidCode;
 import my.weekly.manager.quartz.FuckJob;
 import my.weekly.manager.quartz.HelloJob;
+import my.weekly.manager.quartz.UpdateDemandStatus;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 
@@ -13,6 +15,8 @@ public enum QuartzInfo {
 
     HELLOWORLD(HelloJob.class, "hello_job","hello_trigger", MyCronInfo.PER_FIVE_SEC) {},
     FUCKWORLD(FuckJob.class, "fuck_job","fuck_trigger", MyCronInfo.PER_FIVE_SEC) {},
+    CLEARINVALIDCODE(ClearInvalidCode.class, "clear_invalid_code_job", "clear_invalid_code_trigger", MyCronInfo.PER_TEN_MINUTE) {},
+    UPDATEDEMANDSTATUS(UpdateDemandStatus.class, "update_demand_status_job","update_demand_status_trigger", MyCronInfo.PER_ONE_HOUR) {},
     ;
 
     @Getter
