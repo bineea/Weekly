a4abc2b9e9662290484c9978c8b79b4854288b59
bineea <33547731+bineea@users.noreply.github.com>
2019-08-27 01:40:55 +0800

    按照更新时间分页查询

--- src/main/java/my/weekly/dao/repo/Spe/WeeklyDemandPageSpe.java
+++ src/main/java/my/weekly/dao/repo/Spe/WeeklyDemandPageSpe.java
@@ -1,11 +1,14 @@
 package my.weekly.dao.repo.Spe;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.criteria.Predicate;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
@@ -13,9 +16,21 @@ import my.weekly.dao.entity.Demand;
 import my.weekly.dao.entity.dict.HandleStatus;
 
 public class WeeklyDemandPageSpe extends AbstractPageSpecification<Demand> {
+	@Getter
+	@Setter
 	private String projectId;
+	@Getter
+	@Setter
 	private HandleStatus handleStatus;
+	@Getter
+	@Setter
 	private List<HandleStatus> handleStatues;
+	@Getter
+	@Setter
+	private LocalDateTime startUpTime;
+	@Getter
+	@Setter
+	private LocalDateTime endUpTime;
 	
 	@Override
 	public Specification<Demand> handleSpecification() {
@@ -27,35 +42,15 @@ public class WeeklyDemandPageSpe extends AbstractPageSpecification<Demand> {
 				predicateList.add(criteriaBuilder.equal(root.get("handleStatus"), handleStatus));
 			if(handleStatues != null && !handleStatues.isEmpty())
 				predicateList.add(criteriaBuilder.in(root.get("handleStatus")).value(handleStatues));
+			if(startUpTime != null)
+				predicateList.add(criteriaBuilder.greaterThanOrEqualTo(root.get("updateTime").as(LocalDateTime.class), startUpTime));
+			if(endUpTime != null)
+				predicateList.add(criteriaBuilder.lessThanOrEqualTo(root.get("updateTime").as(LocalDateTime.class), endUpTime));
 			query.where(predicateList.stream().toArray(Predicate[]::new));
 			query.orderBy(criteriaBuilder.desc(root.get("createTime").as(LocalDateTime.class)), criteriaBuilder.desc(root.get("id").as(String.class)));
 			return query.getRestriction();
 		};
 		return spe;
-	}
-
-	public String getProjectId() {
-		return projectId;
-	}
-
-	public void setProjectId(String projectId) {
-		this.projectId = projectId;
-	}
-
-	public HandleStatus getHandleStatus() {
-		return handleStatus;
-	}
-
-	public void setHandleStatus(HandleStatus handleStatus) {
-		this.handleStatus = handleStatus;
-	}
-
-	public List<HandleStatus> getHandleStatues() {
-		return handleStatues;
-	}
-
-	public void setHandleStatues(List<HandleStatus> handleStatues) {
-		this.handleStatues = handleStatues;
 	}
 
 }
