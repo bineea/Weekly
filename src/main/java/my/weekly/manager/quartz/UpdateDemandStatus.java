6b6585df378f10647ee8b0de07062f28c1c8b9b7
bineea <33547731+bineea@users.noreply.github.com>
2019-08-27 01:40:26 +0800

    更新需求状态定时任务

---
+++ src/main/java/my/weekly/manager/quartz/UpdateDemandStatus.java
@@ -0,0 +1,41 @@
+package my.weekly.manager.quartz;
+
+import my.weekly.dao.entity.Demand;
+import my.weekly.dao.entity.dict.HandleStatus;
+import my.weekly.dao.repo.Spe.WeeklyDemandPageSpe;
+import my.weekly.manager.AbstractManager;
+import my.weekly.manager.daily.DemandManager;
+import my.weekly.model.MyFinals;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Component
+public class UpdateDemandStatus extends AbstractManager implements Job {
+
+    @Autowired
+    private DemandManager demandManager;
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        logger.info("-------------------update demand status start------------------------");
+        WeeklyDemandPageSpe spe = new WeeklyDemandPageSpe();
+        spe.setPageNo(0);
+        spe.setPageSize(1000);
+        spe.setEndUpTime(LocalDateTime.now());
+        Page<Demand> page = demandManager.pageQuery(spe);
+        do {
+            for(Demand demand : page.getContent()) {
+                demandManager.updateStatus(demand);
+            }
+            page = demandManager.pageQuery(spe);
+        } while (page.getTotalElements() > 0);
+        logger.info("-------------------update demand status end------------------------");
+    }
+}
